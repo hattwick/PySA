c952aa48d0f8a8bb9fa846d515c5d3ae971f018c
Phil Hattwick <hattwick@lucent.com>
2012-07-04 00:16:11 -0400

    Comment from phil pycharm commit gui

---
+++ scratch.py
@@ -0,0 +1,24 @@
+"""
+Problem: Print out user-provided text
+Target Users: testers
+Interface: Command line
+Functional Requirements:  Prompt for user input
+													Process input and respond
+Testing: Editor test and then execute separately
+Maintainer: philhatt@mac.com
+"""
+
+# Text string above is initial Python docstring
+
+# Welcome message
+
+print ("Python input program (c) 2012")
+
+
+# Get input
+
+text = input('Type in some words: ')
+
+# Echo output
+
+print(text)
