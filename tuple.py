0c4e0be190609fbbf702448ecd72ba5bf74b1763
hattwick <phil@hattwick.com>
2015-09-16 23:04:18 -0400

    initial new

---
+++ tuple.py
@@ -0,0 +1,20 @@
+__author__ = 'phil'
+
+# exercises from Data Science from Scrath
+# code may or may not mirror the text examples
+
+# data set being analyzed is a list of users and connections
+
+users = [
+    {"id":0,"name": "hero"},
+    {"id":1, "name": "Dunn"},
+    {"id":2, "name": "Sue"},
+    {"id":3, "name": "Chi"},
+    {"id":4, "name": "Thor"},
+    {"id":5, "name": "Clive"},
+    {"id":6, "name": "Hicks"},
+    {"id":7, "name": "Devin"},
+    {"id":8, "name": "Kate"},
+    {"id":9, "name": "Klein"}
+]
+
