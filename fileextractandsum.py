0ab2cfe47dd304410511755b2e87840c8cc38818
hattwick <phil@hattwick.com>
2015-12-29 20:34:32 -0500

    combine list, make unique via set and sort

--- fileextractandsum.py
+++ fileextractandsum.py
@@ -16,3 +16,21 @@ for line in fh:
 
 confsum=sum(confidencerepo)
 print 'Average spam confidence:', confsum / len(confidencerepo)
+
+
+
+# second pass to print email addresses
+
+fname = raw_input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if not line.startswith("From:") : continue
+    subsplit = line.split()
+    print (subsplit[1])
+    count += 1
+
+print 'There were',count, 'lines in the file with From as the first word'
