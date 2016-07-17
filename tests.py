37d7d5a3cce1234907b31ac4282cbca76dee2e90
hattwick <phil@hattwick.com>
2016-07-16 22:37:10 -0400

    new unittest beginning

---
+++ tests.py
@@ -0,0 +1,15 @@
+# Sample python collection of unittest concepts
+
+def get_formatted_name(first,last):
+    """receive name and generate neatly-formatted full name"""
+    full_name = first + ' ' + last
+    return full_name.title()
+
+print('q to quit.')
+while True:
+    first = input('\nWhat is your first name? ')
+    if first == 'q':
+        break
+    last = input('\nWhat is your last name? ')
+    if last == 'q':
+        break
