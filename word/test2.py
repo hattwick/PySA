ed3f3abf073abe73a93f050e78b1ded7f1ca9c91
hattwick <phil@hattwick.com>
2016-09-18 22:19:40 -0400

    palindrome check

---
+++ word/test2.py
@@ -0,0 +1,9 @@
+# Short palindrome finder
+
+import string
+print(string.ascii_lowercase)
+
+# test long palindrome
+word = "rotavator"
+print('\nIs rotavator a palindrom?\n')
+print(word == word[::-1])
