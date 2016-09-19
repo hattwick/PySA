56995785b45a2529b9c6e06a968ecac882a07493
hattwick <phil@hattwick.com>
2016-09-18 22:22:59 -0400

    updated final palndrome on test2.py

--- word/test2.py
+++ word/test2.py
@@ -1,9 +1,20 @@
 # Short palindrome finder
 
 import string
+import scrabble
+
 print(string.ascii_lowercase)
 
 # test long palindrome
 word = "rotavator"
 print('\nIs rotavator a palindrom?\n')
 print(word == word[::-1])
+
+# Now the full check
+
+longest = ""
+
+for word in scrabble.wordlist:
+    if word == word[::-1] and len(word) > len(longest):
+        longest = word
+print('After checking the scrabble file, the longest word is', longest)
