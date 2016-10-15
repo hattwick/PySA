4e98a36c6389060d51f0aff0e72644c004748494
hattwick <phil@hattwick.com>
2016-10-14 23:14:09 -0400

    intermed python - using sonnets to test speed

---
+++ word/sonnets.py
@@ -0,0 +1,23 @@
+import time
+
+my_words = [elt.strip() for elt in open("sonnet_words.txt", "r").readlines()]
+word_list = [elt.strip() for elt in open("sowpods.txt", "r").readlines()]
+
+# we speed up the search by adding the set
+word_set = dict((elt, 1) for elt in word_list)
+word_set2 = set(word_list)
+
+
+counter = 0
+
+start = time.time()
+
+for word in my_words:
+    if word not in word_set2:    #play with this variable to infuence speed using dict, set, list
+        print(word)
+        counter += 1
+
+stop = time.time()
+
+print("Total new words: %d" % counter)
+print("Elapsed time: %.1f seconds" %(stop-start))
