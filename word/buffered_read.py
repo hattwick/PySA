16290387be0cd66ea3f5f0e9ed6ca39046fa0fb1
hattwick <phil@hattwick.com>
2016-11-20 13:40:55 -0500

    added unbuffered function

--- word/buffered_read.py
+++ word/buffered_read.py
@@ -2,7 +2,14 @@
 # Based on exercise files from Python3 Essential Training
 # Goal is to evolve this to a reusable module
 
-def main():
+def unbuffered():
+    file_in = open('file.txt', 'r')
+    file_out = open('newfile.txt', 'w')
+    for line in file_in:
+        print(line, file = file_out, end ='')
+    print('Unbuffered read has been save in newfile.txt.')
+
+def buffered():
     buffersize = 50000
     file_in = open('file.txt', 'r')
     file_out = open('newfile.txt', 'w')
@@ -11,3 +18,5 @@ def main():
         file_out.write(buffer)
         print('.', end = '')
         buffer = file_in.read(buffersize)
+
+if __name__ == "__main__":  buffered()
