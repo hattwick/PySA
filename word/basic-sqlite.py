d33390f321a3b27e4526b43a070367a46ba5a230
hattwick <phil@hattwick.com>
2016-11-20 21:03:16 -0500

    add basic sqllite utility

---
+++ word/basic-sqlite.py
@@ -0,0 +1,54 @@
+#!/user/bin/python3
+# Basic sqlite operations modelled from Python3 Essential Training
+# Not production quality - no error checking
+
+import sqlite3
+
+def insert(db, row):
+    db.execute('insert into test (t1, i1) value (?, ?)', (row['t1'], row['i1']))
+    db.commit()
+
+def retrieve(db, t1):
+    cursor = db.execute('select * from test where t1 = ?', (t1,))
+    return cursor.fetchone()
+
+def update(db, row):
+    db.execute('update test set i1 = ? where t1 = ?', (row['i1'], row['t1']))
+    db.commit()
+
+def delete(db, t1):
+    db.execute('delete from test where t1 = ?', (t1,))
+    db.commit()
+
+def display_row(db):
+    cursor = db.execute('select * from test order by t1')
+    for row in cursor:
+        print(' {}:{}'.format(row['t1'], row['i1']))
+
+
+def main():
+    db = sqlite3.connect('test.db')
+    db.row_factory = sqlite3.Row
+    print('Creating table test')
+    print('First removing old test. . .')
+    db.execute('drop table if exists test')
+    db.execute('create table test (t1 text, i1 int )')
+    print('New test table created')
+    print('Creating test data')
+    insert(db, dict(t1 = 'one', i1 = 1))
+    insert(db, dict(t1 = 'two', i1 = 2))
+    insert(db, dict(t1 = 'three', i1 = 3))
+    insert(db, dict(t1 = 'four', i1 = 4))
+    display_row(db)
+
+    print('Retrieving rows . . . . .')
+    print(dict(retrieve(db,'one')), dict(retrieve(db, 'two')))
+
+    print('Updating row . . . . .')
+    update(db, dict(t1 = 'one', i1 = 111))
+    display_row(db)
+
+
+
+
+if __name__ == "__main__": main()
--- word/buffered_read.py
+++ word/buffered_read.py
@@ -1,4 +1,4 @@
-# Python3 module for buffered read
+# Python3 module for various file reads
 # Based on exercise files from Python3 Essential Training
 # Goal is to evolve this to a reusable module
 
@@ -9,7 +9,7 @@ def unbuffered():
         print(line, file = file_out, end ='')
     print('Unbuffered read has been save in newfile.txt.')
 
-def buffered():
+def buffered():     # read and write in buffered IO mode.  Need to rewrite an interable version
     buffersize = 50000
     file_in = open('file.txt', 'r')
     file_out = open('newfile.txt', 'w')
@@ -18,5 +18,17 @@ def buffered():
         file_out.write(buffer)
         print('.', end = '')
         buffer = file_in.read(buffersize)
+
+def binary():     # binary reads must use buffered IO.
+    buffersize = 50000
+    file_in = open('file.tar', 'rb')
+    file_out = open('newfile.txt', 'wb')
+    buffer = file_in.read(buffersize)
+    while len(buffer):
+        file_out.write(buffer)
+        print('.', end = '')
+        buffer = file_in.read(buffersize)
+
+
 
 if __name__ == "__main__":  buffered()
